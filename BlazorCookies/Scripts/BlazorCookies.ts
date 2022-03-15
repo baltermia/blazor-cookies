38aa027b2a3df76a0f922b61da55c98274f59a6b
speyck <54800864+speyck@users.noreply.github.com>
2022-03-15 20:55:00 +0100

    Add ts Script and compiled js file

---
+++ BlazorCookies/Scripts/BlazorCookies.ts
@@ -0,0 +1,5 @@
+﻿namespace BlazorCookies.Scripts {
+    export class BlazorCookiesInterop {
+        
+    }
+}
---
+++ BlazorCookies/wwwroot/js/BlazorCookies.js
@@ -0,0 +1,10 @@
+"use strict";
+var BlazorCookies;
+(function (BlazorCookies) {
+    var Scripts;
+    (function (Scripts) {
+        class BlazorCookiesInterop {
+        }
+        Scripts.BlazorCookiesInterop = BlazorCookiesInterop;
+    })(Scripts = BlazorCookies.Scripts || (BlazorCookies.Scripts = {}));
+})(BlazorCookies || (BlazorCookies = {}));
