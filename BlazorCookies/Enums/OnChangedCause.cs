2efdc35d48ab7f557b215c271a8391e090893cb4
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-10 21:03:25 +0200

    Create OnChangedCause enum

---
+++ BlazorCookies/Enums/OnChangedCause.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCookies.Services;
+
+namespace BlazorCookies.Enums;
+
+/// <summary>
+/// Enum representing the underlying reason behind a <see cref="ICookieService.OnChanged"/> event fire.
+/// </summary>
+public enum OnChangedCause
+{
+    Evicted,
+    Expired,
+    Explicit,
+    Expired_Overwrite,
+    Overwrite
+}
