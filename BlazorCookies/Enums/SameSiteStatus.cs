864a6a61939650aee41e934a3fdf2fc35a5c5056
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-10 21:36:05 +0200

    Add enum docs for values

--- BlazorCookies/Enums/SameSiteStatus.cs
+++ BlazorCookies/Enums/SameSiteStatus.cs
@@ -1,8 +1,15 @@
-﻿namespace BlazorCookies.Enums;
+﻿using BlazorCookies.Models;
+
+namespace BlazorCookies.Enums;
 
 public enum SameSiteStatus
 {
+    /// <summary>Represents a <see cref="Cookie"/> set without a <see cref="Cookie.SameSite"/> attribute.</summary>
     NoRestriction,
+
+    /// <summary>Corresponds to <code>SameSite=Lax</code></summary>
     Lax,
+
+    /// <summary>Corresponds to <code>SameSite=Strict</code></summary>
     Strict
 }
