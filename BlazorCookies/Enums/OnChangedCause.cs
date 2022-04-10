c020ac39379608f97c0affd4593749637192eb71
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-10 21:35:55 +0200

    Add enum docs for values

--- BlazorCookies/Enums/OnChangedCause.cs
+++ BlazorCookies/Enums/OnChangedCause.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorCookies.Services;
+using BlazorCookies.Models;
 
 namespace BlazorCookies.Enums;
 
@@ -7,9 +8,18 @@ namespace BlazorCookies.Enums;
 /// </summary>
 public enum OnChangedCause
 {
+    /// <summary>A <see cref="Cookie"/> has been automatically removed due to garbage collection.</summary>
     Evicted,
+
+    /// <summary>A <see cref="Cookie"/> has been automatically removed due to expiry.</summary>
     Expired,
+
+    /// <summary>A <see cref="Cookie"/> has been inserted or removed via an explicit call to <see cref="ICookieService.RemoveAsync"/>.</summary>
     Explicit,
+
+    /// <summary>A <see cref="Cookie"/> has been overwritten by a cookie with an already-expired expiration date.</summary>
     Expired_Overwrite,
+
+    /// <summary>A call to <see cref="ICookieService.SetAsync"/> overwrote this <see cref="Cookie"/> with a different one.</summary>
     Overwrite
 }
