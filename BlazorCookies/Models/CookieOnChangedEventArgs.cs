f0b40e94d3cca201f427f61e2b2bdd354de6cd4c
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-10 21:03:03 +0200

    Create CookieOnChangedEventArgs class

---
+++ BlazorCookies/Models/CookieOnChangedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using BlazorCookies.Enums;
+using BlazorCookies.Services;
+
+namespace BlazorCookies.Models;
+
+/// <summary>
+/// Class representing EventArgs that are sent when a <see cref="ICookieService.OnChanged" /> event is fired
+/// </summary>
+public class CookieOnChangedEventArgs
+{
+    /// <summary>
+    /// A boolean that is set to true if a cookie was removed, and false if not.
+    /// </summary>
+    public bool Removed { get; set; }
+
+    /// <summary>
+    /// A <see cref="Models.Cookie"/> object containing information about the cookie that was set or removed.
+    /// </summary>
+    public Cookie Cookie { get; set; }
+
+    /// <summary>
+    /// A <see cref="OnChangedCause"/> value representing the underlying reason behind the <see cref="Models.Cookie"/> objects change.
+    /// </summary>
+    public OnChangedCause Cause { get; set; }
+}
