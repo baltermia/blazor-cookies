4b64d3cea5162958bdd1a858253a663140ed6ced
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-10 20:29:14 +0200

    Add GetAll Method with CookieStore

--- BlazorCookies/Services/ICookieService.cs
+++ BlazorCookies/Services/ICookieService.cs
@@ -20,4 +20,11 @@ public interface ICookieService
     /// <param name="storeId">String representing the ID of the cookie store in which to look for the cookie (as returned by cookies.getAllCookieStores()). By default, the current execution context's cookie store will be used.</param>
     /// <returns>A single <see cref="Cookie" />, given its name and URL.</returns>
     public Cookie Get(string url, string name, string storeId = null);
+
+    /// <summary>
+    /// Retrieves all <see cref="Cookie" /> objects from a single <see cref="CookieStore" /> that match the given information.
+    /// </summary>
+    /// <param name="store">The <see cref="CookieStore" /> that contains the <see cref="Cookie" /> objects to be returned</param>
+    /// <returns>A list of <see cref="Cookie" /> objects that are in the given <see cref="CookieStore"/>. Only unexpired cookies are returned. The cookies returned will be sorted by path length, longest to shortest. If multiple cookies have the same path length, those with the earliest creation time will be first.</returns>
+    public IEnumerable<Cookie> GetAll(CookieStore store);
 }
