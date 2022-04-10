5d6cb34352d7a7f6c2f427f428ab7b0e977586f5
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-10 21:21:25 +0200

    Add GetAllCookieStores method

--- BlazorCookies/Services/ICookieService.cs
+++ BlazorCookies/Services/ICookieService.cs
@@ -21,24 +21,27 @@ public interface ICookieService
     public event OnChangedEventHandler OnChanged;
 
     /// <summary>
-    /// Retrieves a single <see cref="CookieStore" />, given its name and URL.
+    /// Retrieves a single <see cref="Cookie" />, given its name and URL.
     /// 
+    /// <para>
     /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
     /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
     /// If no matching Cookie could be found, null is returned.
+    /// </para>
     /// </summary>
     /// <param name="url">String representing the URL with which the Cookie to retrieve is associated. This argument may be a full URL, in which case any data following the URL path (e.g. the query string) is ignored.</param>
     /// <param name="name">String representing the name of the cookie to retrieve.</param>
-    /// <param name="storeId">String representing the ID of the cookie store in which to look for the cookie (as returned by cookies.getAllCookieStores()). By default, the current execution context's cookie store will be used.</param>
+    /// <param name="storeId">String representing the ID of the cookie store in which to look for the cookie (as returned by <see cref="GetAllCookieStores"/>). By default, the current execution context's cookie store will be used.</param>
     /// <returns>A single <see cref="Cookie" />, given its name and URL.</returns>
     public Cookie Get(string url, string name, string storeId = null);
 
     /// <summary>
-    /// Retrieves a single <see cref="CookieStore" />, given its name and URL.
-    /// 
+    /// Retrieves a single <see cref="Cookie" />, given its <see cref="CookieDetails"/>.
+    /// <para>
     /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
     /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
     /// If no matching Cookie could be found, null is returned.
+    /// </para>
     /// </summary>
     /// <param name="details"><see cref="CookieDetails"/> object containing details that can be used to match cookies to be retrieved.</param>
     /// <returns>A single <see cref="Cookie" />, given its <see cref="CookieDetails"/></returns>
@@ -57,4 +60,10 @@ public interface ICookieService
     /// <param name="details"><see cref="CookieDetails"/> object containing details that can be used to match cookies to be retrieved.</param>
     /// <returns>A list of <see cref="Cookie" /> objects that match the parameters. Only unexpired cookies are returned. The cookies returned will be sorted by path length, longest to shortest. If multiple cookies have the same path length, those with the earliest creation time will be first.</returns>
     public IEnumerable<Cookie> GetAll(CookieDetails details);
+
+    /// <summary>
+    /// Returns a <see cref="IEnumerable{CookieStore}"/> list of all <see cref="CookieStore"/> objects.
+    /// </summary>
+    /// <returns>All accessible <see cref="CookieStore"/> objects</returns>
+    public IEnumerable<CookieStore> GetAllCookieStores();
 }
