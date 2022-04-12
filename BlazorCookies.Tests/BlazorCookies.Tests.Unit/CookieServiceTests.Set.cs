23237287cd64e7d7aa10a69c4a3ea2fe3c05aabb
baltermia <54800864+baltermia@users.noreply.github.com>
2022-04-12 19:33:55 +0200

    Add default structure and BlazorCookies package for testing

--- BlazorCookies.Tests/BlazorCookies.Tests.Unit/CookieServiceTests.Set.cs
+++ BlazorCookies.Tests/BlazorCookies.Tests.Unit/CookieServiceTests.Set.cs
@@ -1,7 +1,13 @@
-﻿namespace BlazorCookies.Tests.Unit
+﻿using NUnit.Framework;
+
+namespace BlazorCookies.Tests.Unit
 {
     public partial class CookieServiceTests
     {
+        [Test]
+        public void Set_NameOnly()
+        {
 
+        }
     }
 }
