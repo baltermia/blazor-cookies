b195a9371a0f35c0931b8256732a58dafdf214d7
baltermia <54800864+baltermia@users.noreply.github.com>
2023-04-28 11:29:12 +0200

    Format code

--- BlazorCookies.Tests/BlazorCookies.Tests.Unit/CookieServiceTests.GetSet.cs
+++ BlazorCookies.Tests/BlazorCookies.Tests.Unit/CookieServiceTests.GetSet.cs
@@ -2,29 +2,28 @@
 using NUnit.Framework;
 using System.Threading.Tasks;
 
-namespace BlazorCookies.Tests.Unit
+namespace BlazorCookies.Tests.Unit;
+
+public partial class CookieServiceTests
 {
-    public partial class CookieServiceTests
+    [Test]
+    public async Task GetSet_NameOnly()
     {
-        [Test]
-        public async Task GetSet_NameOnly()
+        // Arrange
+        CookieDetails details = new()
         {
-            // Arrang
-            CookieDetails details = new()
-            {
-                Name = "test-getset_nameonly"
-            };
+            Name = "test-getset_nameonly"
+        };
 
-            string value = "test-getset_nameonly-value";
+        string value = "test-getset_nameonly-value";
 
-            // Act
-            Cookie cookieSet = await cookieService.SetAsync(details, value);
+        // Act
+        Cookie cookieSet = await cookieService.SetAsync(details, value);
 
-            Cookie cookieGet = await cookieService.GetAsync(details);
+        Cookie cookieGet = await cookieService.GetAsync(details);
 
-            // Assert
-            Assert.That(value, Is.EqualTo(cookieGet.Value));
-            Assert.That(cookieGet, Is.EqualTo(cookieSet));
-        }
+        // Assert
+        Assert.That(value, Is.EqualTo(cookieGet.Value));
+        Assert.That(cookieGet, Is.EqualTo(cookieSet));
     }
 }
--- BlazorCookies.Tests/BlazorCookies.Tests.Unit/CookieServiceTests.cs
+++ BlazorCookies.Tests/BlazorCookies.Tests.Unit/CookieServiceTests.cs
@@ -1,16 +1,15 @@
 ﻿using NUnit.Framework;
 using BlazorCookies.Services;
 
-namespace BlazorCookies.Tests.Unit
+namespace BlazorCookies.Tests.Unit;
+public partial class CookieServiceTests
 {
-    public partial class CookieServiceTests
-    {
-        private ICookieService cookieService;
+    private readonly ICookieService cookieService;
 
-        [SetUp]
-        public void Setup()
-        {
-            // TODO: Create new object of ICookieService interface implementation to cookieService
-        }
+    [SetUp]
+    public void Setup()
+    {
+        // TODO: Create new object of ICookieService interface implementation to cookieService
     }
 }
+
