3dcb3f9fabdda01099a202ee51c29bfd960bf16a
baltermia <54800864+baltermia@users.noreply.github.com>
2023-04-28 11:51:42 +0200

    Add DotNetObjectReference interface in ts for .NET-Invokes

--- BlazorCookies/Scripts/BlazorCookies.ts
+++ BlazorCookies/Scripts/BlazorCookies.ts
@@ -1,4 +1,16 @@
 ﻿namespace BlazorCookies.Scripts {
+    /**
+     * Interface representing a DotNetObjekt
+     */
+    interface DotNetObjectReference {
+        /**
+         * Invokes the method specified with the identifier on the dotnet object with the given arguments
+         * @param identifier The identifier of the JSInvokable method in the dotnet object
+         * @param args The arguments that are given when invoking the method
+         */
+        invokeMethodAsync(identifier: string, ...args: any): any;
+    }
+
     export class BlazorCookiesInterop {
         
     }
