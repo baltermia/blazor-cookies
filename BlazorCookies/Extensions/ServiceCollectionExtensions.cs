d70171729abdb9521d08a3c11d494b679b8ed1f6
baltermia <54800864+baltermia@users.noreply.github.com>
2023-04-28 14:14:43 +0200

    Create ServiceCollectionExtensions class with AddBlazorCookies method

---
+++ BlazorCookies/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using BlazorCookies.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorCookies;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBlazorCookies(this IServiceCollection services) =>
+        services.AddScoped<ICookieService, CookieService>();
+}
