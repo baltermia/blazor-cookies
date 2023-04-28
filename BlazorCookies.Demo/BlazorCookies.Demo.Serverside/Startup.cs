2381e381abb17fba1992f29e3435f140ff8969e0
baltermia <54800864+baltermia@users.noreply.github.com>
2023-04-28 15:05:04 +0200

    Remove unused stuff in Demo projects

--- BlazorCookies.Demo/BlazorCookies.Demo.Serverside/Startup.cs
+++ BlazorCookies.Demo/BlazorCookies.Demo.Serverside/Startup.cs
@@ -1,4 +1,3 @@
-using BlazorCookies.Demo.Serverside.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +21,6 @@ namespace BlazorCookies.Demo.Serverside
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- BlazorCookies.Demo/BlazorCookies.Demo.WASM/Program.cs
+++ BlazorCookies.Demo/BlazorCookies.Demo.WASM/Program.cs
@@ -1,7 +1,4 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BlazorCookies.Demo.WASM
@@ -12,8 +9,6 @@ namespace BlazorCookies.Demo.WASM
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
