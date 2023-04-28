e97062d55dbc6b0fbf3f07ffd50515df5c6eb1a5
baltermia <54800864+baltermia@users.noreply.github.com>
2023-04-28 15:20:27 +0200

    Create CookieInterop class and use in CookieService

---
+++ BlazorCookies/Interop/CookieInterop.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorCookies.Interop;
+
+internal class CookieInterop
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public CookieInterop(IJSRuntime jsRuntime) => _jsRuntime = jsRuntime;
+}
