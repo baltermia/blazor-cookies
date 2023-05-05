cb17c7308be78232cdc639e1fe5d080016f85615
baltermia <54800864+baltermia@users.noreply.github.com>
2023-05-05 10:50:47 +0200

    Adjust Cookie & CookieDetails models to fit document.cookies API

--- BlazorCookies/Models/CookieDetails.cs
+++ BlazorCookies/Models/CookieDetails.cs
@@ -1,42 +1,40 @@
-﻿namespace BlazorCookies.Models;
+﻿using System;
+
+namespace BlazorCookies.Models;
 
 /// <summary>
-/// Class representing details used to get/set <see cref="Cookie"/> objects.
+/// Class representing details used to set & delete <see cref="Cookie"/> objects.
 /// </summary>
 public class CookieDetails
 {
     /// <summary>
-    /// String representing a domain that <see cref="Cookie" /> objects must be associated with (they can be associated either with this exact domain or one of its subdomains).
-    /// </summary>
-    public string Domain { get; set; }
-
-    /// <summary>
-    /// String representing a name that the <see cref="Cookie" /> objects should have.
+    /// The name of the cookie.
     /// </summary>
     public string Name { get; set; }
 
     /// <summary>
-    /// String representing a path — the <see cref="Cookie" /> objects' path must be identical to this one.
+    /// The domain of the cookie (e.g. "www.google.com" "example.com").
     /// </summary>
-    public string Path { get; set; }
+    public string? Domain { get; set; }
 
+    /// <summary>
+    /// The path of the cookie.
+    /// </summary>
+    public string? Path { get; set; }
+
+    /// <summary>
+    /// Indicates the SameSite state of the cookie.
+    /// </summary>
+    public SameSiteStatus SameSite { get; set; }
+
+    /// <summary>
+    /// The expiration date of the cookie. Not provided for session cookies.
+    /// </summary>
+    public DateTime? Expiration { get; set; }
     /// <summary>
     /// Bool that filters <see cref="Cookie" /> objects by their secure property, allowing you to filter secure cookies vs. non-secure cookies.
     /// </summary>
-    public bool? Secure { get; set; } = null;
+    public bool Secure { get; set; } = false;
 
-    /// <summary>
-    /// Bool that filters the <see cref="Cookie" /> objects by their session property, allowing you to filter session cookies vs. persistent cookies.
-    /// </summary>
-    public bool? Session { get; set; } = null;
-
-    /// <summary>
-    /// String representing the <see cref="CookieStore.Id" /> to retrieve <see cref="Cookie" /> objects from. If omitted, the current execution context's cookie store will be used.
-    /// </summary>
-    public string StoreId { get; set; }
-
-    /// <summary>
-    /// String representing a URL that the retrieved <see cref="Cookie" /> objects must be associated with.
-    /// </summary>
-    public string Url { get; set; }
+    public CookieDetails(string name) => Name = name;
 }
