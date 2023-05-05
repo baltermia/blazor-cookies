7b94891549d7d475c7c4e1c5afb1b576b6bc95dd
baltermia <54800864+baltermia@users.noreply.github.com>
2023-05-05 10:52:14 +0200

    Adjust ICookieService to fit document.cookies API

--- BlazorCookies/Services/CookieService.cs
+++ BlazorCookies/Services/CookieService.cs
@@ -1,7 +1,5 @@
-﻿using BlazorCookies.Interop;
-using BlazorCookies.Models;
+﻿using BlazorCookies.Models;
 using BlazorCookies.Repositories;
-using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -28,64 +26,31 @@ internal class CookieService : ICookieService
 {
     private readonly ICookieRepository _repo;
 
-    public string DefaultCookiePath { init; get; }
+    public string? DefaultCookiePath { get; init; }
 
     /// <summary>
     /// Creates a new non-generic CookieSerivce, given a default cookie-path
     /// </summary>
-    public CookieService(ICookieRepository repo, string defaultCookiePath)
+    public CookieService(ICookieRepository repo, string? defaultCookiePath)
     {
         DefaultCookiePath = defaultCookiePath;
         _repo = repo;
     }
 
-    public Cookie this[CookieDetails details] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public Cookie this[string name, string url = null, string storeId = null] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public Task<Cookie> this[CookieDetails details] => GetAsync(details);
 
-    public event ICookieService.OnChangedEventHandler OnChanged;
+    public Task<Cookie> this[string name] => GetAsync(name);
 
-    public Task<IEnumerable<Cookie>> GetAllAsync(CookieStore store)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Cookie> GetAsync(string name) => throw new NotImplementedException();
 
-    public Task<IEnumerable<Cookie>> GetAllAsync(CookieDetails details)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Cookie> GetAsync(CookieDetails details) => GetAsync(details.Name);
 
-    public Task<IEnumerable<CookieStore>> GetAllCookieStoresAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public Task<IEnumerable<Cookie>> GetAllAsync() => throw new NotImplementedException();
+    public Task<Cookie> SetAsync(string name, string value) => SetAsync(new CookieDetails(name), value);
 
-    public Task<Cookie> GetAsync(string name, string url = null, string storeId = null)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Cookie> SetAsync(CookieDetails details, string value) => throw new NotImplementedException();
 
-    public Task<Cookie> GetAsync(CookieDetails details)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Cookie> RemoveAsync(string name) => RemoveAsync(new CookieDetails(name));
 
-    public Task<Cookie> RemoveAsync(Cookie cookie)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cookie> RemoveAsync(CookieDetails details)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cookie> SetAsync(Cookie cookie)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cookie> SetAsync(CookieDetails details, string value = null)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Cookie> RemoveAsync(CookieDetails details) => throw new NotImplementedException();
 }
--- BlazorCookies/Services/ICookieService.cs
+++ BlazorCookies/Services/ICookieService.cs
@@ -12,24 +12,12 @@ public interface ICookieService<T> : ICookieService { }
 public interface ICookieService
 {
     /// <summary>
-    /// Delegate representing a method that handles <see cref="OnChanged"/> events
-    /// </summary>
-    /// <param name="sender">Object that fired the event</param>
-    /// <param name="e">EventArgs that contain data containing changed Cookies</param>
-    public delegate void OnChangedEventHandler(object sender, CookieOnChangedEventArgs e);
-
-    /// <summary>
-    /// Event that is fired when a <see cref="Cookie"/> that can be accessed is set or removed.
-    /// </summary>
-    public event OnChangedEventHandler OnChanged;
-
-    /// <summary>
     /// The default Path/Url that is used when creating/setting <see cref="Cookie" /> objects and no path/url is given.
     /// <para>
     /// Used in <see cref="GetAsync" />, <see cref="SetAsync" />, <see cref="RemoveAsync" /> and <see cref="this[CookieDetails]" />
     /// </para>
     /// </summary>
-    public string DefaultCookiePath { get; }
+    public string? DefaultCookiePath { get; }
 
     /// <summary>
     /// Retrieves a single <see cref="Cookie" />, given its name and URL.
@@ -37,87 +25,53 @@ public interface ICookieService
     /// <para>
     /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
     /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
-    /// If no matching Cookie could be found, null is returned.
+    /// If no matching Cookie could be found, the Value is null.
     /// </para>
     /// </summary>
     /// <param name="name">String representing the name of the cookie to retrieve.</param>
-    /// <param name="url">String representing the URL with which the Cookie to retrieve is associated. This argument may be a full URL, in which case any data following the URL path (e.g. the query string) is ignored.</param>
-    /// <param name="storeId">String representing the ID of the cookie store in which to look for the cookie (as returned by <see cref="GetAllCookieStoresAsync"/>). By default, the current execution context's cookie store will be used.</param>
-    /// <returns>A single <see cref="Cookie" />, given its name and URL.</returns>
-    public Task<Cookie> GetAsync(string name, string url = null, string storeId = null);
+    public Task<Cookie> GetAsync(string name);
 
     /// <summary>
-    /// Retrieves a single <see cref="Cookie" />, given its <see cref="CookieDetails"/>.
+    /// Retrieves a single <see cref="Cookie" />, given its name and URL.
+    /// 
     /// <para>
     /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
     /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
-    /// If no matching Cookie could be found, null is returned.
+    /// If no matching Cookie could be found, the Value is null.
     /// </para>
     /// </summary>
-    /// <param name="details"><see cref="CookieDetails"/> object containing details that can be used to match cookies to be retrieved.</param>
-    /// <returns>A single <see cref="Cookie" />, given its <see cref="CookieDetails"/></returns>
+    /// <param name="details">Details representing the cookie to retrieve.</param>
     public Task<Cookie> GetAsync(CookieDetails details);
 
     /// <summary>
-    /// Retrieves all <see cref="Cookie" /> objects from a single <see cref="CookieStore" /> that match the given information.
+    /// Gets all <see cref="Cookie" /> objects
     /// </summary>
-    /// <param name="store">The <see cref="CookieStore" /> that contains the <see cref="Cookie" /> objects to be returned</param>
-    /// <returns>A list of <see cref="Cookie" /> objects that are in the given <see cref="CookieStore"/>. Only unexpired cookies are returned. The cookies returned will be sorted by path length, longest to shortest. If multiple cookies have the same path length, those with the earliest creation time will be first.</returns>
-    public Task<IEnumerable<Cookie>> GetAllAsync(CookieStore store);
+    public Task<IEnumerable<Cookie>> GetAllAsync();
 
     /// <summary>
-    /// Retrieves all <see cref="Cookie" /> objects from a single <see cref="CookieStore" /> that match the given (optional) information.
+    /// Sets the underlying cookie matching the name
     /// </summary>
-    /// <param name="details"><see cref="CookieDetails"/> object containing details that can be used to match cookies to be retrieved.</param>
-    /// <returns>A list of <see cref="Cookie" /> objects that match the parameters. Only unexpired cookies are returned. The cookies returned will be sorted by path length, longest to shortest. If multiple cookies have the same path length, those with the earliest creation time will be first.</returns>
-    public Task<IEnumerable<Cookie>> GetAllAsync(CookieDetails details);
+    /// <returns>The <see cref="Cookie"/> that was set</returns>
+    public Task<Cookie> SetAsync(string name, string value);
 
     /// <summary>
-    /// Returns a <see cref="IEnumerable{CookieStore}"/> list of all <see cref="CookieStore"/> objects.
+    /// Sets the underlying cookie matching the <see cref="CookieDetails" />
     /// </summary>
-    /// <returns>All accessible <see cref="CookieStore"/> objects</returns>
-    public Task<IEnumerable<CookieStore>> GetAllCookieStoresAsync();
-
-    /// <summary>
-    /// Sets a <see cref="Cookie" /> containing the specified cookie data. This method is equivalent to issuing an HTTP Set-Cookie header during a request to a given URL.
-    /// </summary>
-    /// <param name="cookie"><see cref="Cookie" /> object containing details of the cookie to be set and the value</param>
-    /// <returns>A <see cref="Task"/> containing a <see cref="Cookie" /> object containing details about the cookie that's been set. If the call fails for any reason, null will be returned.</returns>
-    public Task<Cookie> SetAsync(Cookie cookie);
-
-    /// <summary>
-    /// Sets a <see cref="Cookie" /> containing the specified cookie data. This method is equivalent to issuing an HTTP Set-Cookie header during a request to a given URL.
-    /// </summary>
-    /// <param name="details"><see cref="CookieDetails"/> object containing the details of the cookie you wish to set.</param>
-    /// <param name="value">String representing the value of the cookie. If omitted, this is empty by default.</param>
-    /// <returns>A <see cref="Task"/> containing a <see cref="Cookie" /> object containing details about the cookie that's been set. If the call fails for any reason, null will be returned.</returns>
-    public Task<Cookie> SetAsync(CookieDetails details, string value = null);
-
-    /// <summary>
-    /// Deletes a cookie, given a <see cref="Cookie"/> object.
-    /// </summary>
-    /// <param name="cookie"><see cref="Cookie"/> that should be deleted</param>
-    /// <returns>A <see cref="Task"/> containing a <see cref="Cookie" /> object containing details about the cookie that's been deleted. If the call fails for any reason, null will be returned.</returns>
-    public Task<Cookie> RemoveAsync(Cookie cookie);
+    /// <returns>The <see cref="Cookie"/> that was set</returns>
+    public Task<Cookie> SetAsync(CookieDetails details, string value);
 
     /// <summary>
     /// Deletes a cookie, given its <see cref="CookieDetails"/>.
     /// </summary>
     /// <param name="details"><see cref="CookieDetails" /> specifying the <see cref="Cookie"/> to be deleted.</param>
-    /// <returns>A <see cref="Task"/> containing a <see cref="Cookie" /> object containing details about the cookie that's been deleted. If the call fails for any reason, null will be returned.</returns>
+    /// <returns>The <see cref="Cookie"/> that was deleted</returns>
     public Task<Cookie> RemoveAsync(CookieDetails details);
 
     /// <summary>
-    /// Retrieves a single <see cref="Cookie" />, given its <see cref="CookieDetails"/>.
-    /// <para>
-    /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
-    /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
-    /// If no matching Cookie could be found, null is returned.
-    /// </para>
+    /// Deletes a cookie, given its name
     /// </summary>
-    /// <param name="details"><see cref="CookieDetails"/> object containing details that can be used to match cookies to be retrieved.</param>
-    /// <returns>A single <see cref="Cookie" />, given its <see cref="CookieDetails"/></returns>
-    public Cookie this[CookieDetails details] { get; set; }
+    /// <returns>The <see cref="Cookie"/> that was deleted</returns>
+    public Task<Cookie> RemoveAsync(string name);
 
     /// <summary>
     /// Retrieves a single <see cref="Cookie" />, given its name and URL.
@@ -125,12 +79,21 @@ public interface ICookieService
     /// <para>
     /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
     /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
-    /// If no matching Cookie could be found, null is returned.
+    /// If no matching Cookie could be found, the Value is null.
     /// </para>
     /// </summary>
-    /// <param name="url">String representing the URL with which the Cookie to retrieve is associated. This argument may be a full URL, in which case any data following the URL path (e.g. the query string) is ignored.</param>
+    /// <param name="details">Details representing the cookie to retrieve.</param>
+    public Task<Cookie> this[CookieDetails details] { get; }
+
+    /// <summary>
+    /// Retrieves a single <see cref="Cookie" />, given its name and URL.
+    /// 
+    /// <para>
+    /// If more than one <see cref="Cookie" /> with the same name exists for a given URL, the one with the longest path will be returned. 
+    /// For Cookies with the same path length, the Cookie with the earliest creation time will be returned. 
+    /// If no matching Cookie could be found, the Value is null.
+    /// </para>
+    /// </summary>
     /// <param name="name">String representing the name of the cookie to retrieve.</param>
-    /// <param name="storeId">String representing the ID of the cookie store in which to look for the cookie (as returned by <see cref="GetAllCookieStoresAsync"/>). By default, the current execution context's cookie store will be used.</param>
-    /// <returns>A single <see cref="Cookie" />, given its name and URL.</returns>
-    public Cookie this[string name, string url = null, string storeId = null] { get; set; }
+    public Task<Cookie> this[string name] { get; }
 }
